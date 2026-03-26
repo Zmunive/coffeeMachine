d93404be21d4fbe44befa5b50d13f73a2307261e
zMunive <brianmunive986@gmail.com>
2026-03-26 16:01:50 -0600

    Add initial menu and refactor main.py

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
-import os
 import json
+import os
 
 
-def getDataFromJason(fileName: str): 
-    
+def get_data_from_json(file_name: str):
     ext = ".json"
-    ncoding = "utf-8"
-    readData = "r"
+    encoding = "utf-8"
+    read_mode = "r"
+    path_file = os.path.join(current_path, file_name + ext)
 
-    pathFile =  os.path.join(getCurrentPath,fileName + ext)
-    
-    with open(pathFile,readData,encoding=ncoding) as file:
+    with open(path_file, read_mode, encoding=encoding) as file:
         data = json.load(file)
+
     return data
-    
 
 
+def create_enumerated_list_from_dict(data: dict):
+    return [f"{i}. {name.capitalize()}" for i, name in enumerate(data, start=1)]
 
-getCurrentPath = os.getcwd()
-coinsData = getDataFromJason("coins")
-cookbookData = getDataFromJason("cookbook")
-resourcesData = getDataFromJason("resources")
 
-print(coinsData)
-print(cookbookData)
-print(resourcesData)
+def print_menu(selections: str) -> None:
+    print(
+        f"""
+Coffee Machine.
 
+{selections}
+"""
+    )
+
+
+current_path = os.getcwd()
+coins_data = get_data_from_json("coins")
+cookbook_data = get_data_from_json("cookbook")
+resources_data = get_data_from_json("resources")
+secret_word = "off"
+coffee_selection = "\n".join(create_enumerated_list_from_dict(cookbook_data))
+
+while True:
+    print_menu(coffee_selection)
+    user_selection = input("Selection: ")
+
+    if user_selection.lower() == secret_word:
+        break
